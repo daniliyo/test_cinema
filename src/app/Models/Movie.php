d8d1c912112b712f258f394c7acc789844b8ac75
Daniil <daniliyo.p1@gmail.com>
2023-04-23 14:02:17 +0300

    Add many-to-many relationship to Genre and Movie models.

--- src/app/Models/Movie.php
+++ src/app/Models/Movie.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_date',
+    ];
+
+    public function genres(){
+        return $this->belongsToMany(Genre::class);
+    }
 }
